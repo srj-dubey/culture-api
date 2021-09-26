b2b8f31fc8fa91fa9bf9666642e50b6f022589cc
Suraj Dubey <suraj@wizikey.com>
2021-09-26 14:40:15 +0530

    initial - added a register route

---
+++ index.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client'
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import cors from 'cors'
+
+//Routes import
+import  router  from './routes'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // Connect the client
+  await prisma.$connect()
+  // ... you will write your Prisma Client queries here
+}
+
+const app = express();
+app.use(cors())
+app.use(json())
+app.use(urlencoded({
+    extended: true
+}))
+
+//Route Middleware
+app.use(router)
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
+
+//Creating Server on the following port
+app.listen(process.env.PORT || 5000)
---
+++ validation/index.ts
@@ -0,0 +1,27 @@
+//Validation for regestering a new user
+
+import Joi from '@hapi/joi'
+
+//register a new user
+
+export const registerUser = (data: any) => {
+
+    const schema  = Joi.object({
+        name: Joi.string()
+            .min(4)
+            .max(120)
+			.required(),
+        email: Joi.string()
+            .min(6)
+            .max(60)
+            .email()
+			.required(),
+		password: Joi.string()
+            .min(8)
+            .max(30)
+            .required(),
+    })
+
+    return schema.validate(data)
+
+}

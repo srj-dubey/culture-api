37c0ce8ee9f0c4e76dff49f9add7803e7277c56d
srj-dubey <surajdub111@gmail.com>
2022-01-15 02:26:44 +0530

    console message cleanup

--- index.ts
+++ index.ts
@@ -1,40 +1,42 @@
-import { PrismaClient } from '@prisma/client'
-import express from 'express'
-import { json, urlencoded } from 'body-parser'
-import cors from 'cors'
-import { User} from './models/user/index'
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+import { json, urlencoded } from "body-parser";
+import cors from "cors";
+import { User } from "./models/user/index";
 
 //Routes import
-import  router  from './routes'
+import router from "./routes";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-  // Connect the client
-	await prisma.$connect()
-	console.log("Database connected!!")
-  // ... you will write your Prisma Client queries here
+	// Connect the client
+	await prisma.$connect().then(() => console.log("Database connected!"));
+	// ... you will write your Prisma Client queries here
 }
 
 const app = express();
-app.use(cors())
-app.use(json())
-app.use(urlencoded({
-    extended: true
-}))
+app.use(cors());
+app.use(json());
+app.use(
+	urlencoded({
+		extended: true,
+	})
+);
 
 //Route Middleware
-app.use(router)
+app.use(router);
 
 main()
-  .catch((e) => {
-    throw e
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+	.catch((e) => {
+		throw e;
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
 
 //Creating Server on the following port
-app.listen(() => {
-	console.log("Port running",process.env.PORT || 5000)
-})
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+	console.log(`Server running -> ${port}`);
+});
